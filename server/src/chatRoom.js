fe4d61f5a1a64202fb56407281f5d7288cd74879
OleksandrDemian <oleksandrovsky@gmail.com>
2019-12-15 21:14:07 +0100

    Added actions to chatRoom

--- server/src/chatRoom.js
+++ server/src/chatRoom.js
@@ -3,6 +3,7 @@ const User = require("./User");
 const chatRoom = {
 	users: [],
 	messages: [],
+	actions: {},
 
 	createUser(connection){
 		this.users.push(new User(connection, this));
@@ -39,32 +40,68 @@ const chatRoom = {
 		return data;
 	},
 
+	action(event, callback){
+		this.actions[event] = callback;
+	},
+
 	on (e, user){
-		switch (e._event) {
-			case "message":
-				const message = e._payload;
-				message.id = new Date().getTime();
-				this.addMessage(message);//auto broadcast
-				break;
-			case "join":
-				user.setData(e._payload);
-				user.active = true;
-				this.broadcast("user_joined", e._payload);
-				this.broadcast("users", this.getUsersData());
-				break;
-			case "leave":
-				user.active = false;
-				this.broadcast("user_left", user.data);
-				this.broadcast("users", this.getUsersData());
-				break;
-			case "requestMessages":
-				user.emit("messages", this.messages);
-				break;
-			case "requestUsers":
-				user.emit("users", this.getUsersData());
-				break;
-		}
+		const { _event, _payload } = e;
+		this.actions[_event]({
+			chatRoom,
+			user
+		}, _payload);
+		// switch (e._event) {
+		// 	case "message":
+		// 		const message = e._payload;
+		// 		message.id = new Date().getTime();
+		// 		this.addMessage(message);//auto broadcast
+		// 		break;
+		// 	case "join":
+		// 		user.setData(e._payload);
+		// 		user.active = true;
+		// 		this.broadcast("user_joined", e._payload);
+		// 		this.broadcast("users", this.getUsersData());
+		// 		break;
+		// 	case "leave":
+		// 		user.active = false;
+		// 		this.broadcast("user_left", user.data);
+		// 		this.broadcast("users", this.getUsersData());
+		// 		break;
+		// 	case "requestMessages":
+		// 		user.emit("messages", this.messages);
+		// 		break;
+		// 	case "requestUsers":
+		// 		user.emit("users", this.getUsersData());
+		// 		break;
+		// }
 	}
 };
+
+chatRoom.action("message", function ({user, chatRoom}, data) {
+	const message = data;
+	message.id = new Date().getTime();
+	chatRoom.addMessage(message);//auto broadcast
+});
+
+chatRoom.action("join", function ({user, chatRoom}, data) {
+	user.setData(data);
+	user.active = true;
+	chatRoom.broadcast("user_joined", data);
+	chatRoom.broadcast("users", chatRoom.getUsersData());
+});
+
+chatRoom.action("leave", function ({user, chatRoom}) {
+	user.active = false;
+	chatRoom.broadcast("user_left", user.data);
+	chatRoom.broadcast("users", chatRoom.getUsersData());
+});
+
+chatRoom.action("requestMessages", function ({user, chatRoom}) {
+	user.emit("messages", chatRoom.messages);
+});
+
+chatRoom.action("requestUsers", function ({user, chatRoom}) {
+	user.emit("users", chatRoom.getUsersData());
+});
 
 module.exports = chatRoom;
